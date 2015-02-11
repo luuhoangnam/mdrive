9ccf47699ef69b9d0e9bea8464256c1e02f2af82
Nam Hoang Luu <nam@mbearvn.com>
2015-02-11 08:31:10 +0700

    Add service provider

---
+++ src/Providers/DriveServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace mBear\Drive\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class DriveServiceProvider
+ *
+ * @author  Nam Hoang Luu <nam@mbearvn.com>
+ * @package mBear\mDrive\Providers
+ *
+ */
+class DriveServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
